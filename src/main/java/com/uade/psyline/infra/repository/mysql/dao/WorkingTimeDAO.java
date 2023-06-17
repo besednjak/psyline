a23163b5cca5bb30d5605fb76c921d473e8d7744
tmerchan00 <tamara.m94@hotmail.com>
2023-06-17 17:38:52 -0300

    Added WorkingTimeDAO

---
+++ src/main/java/com/uade/psyline/infra/repository/mysql/dao/WorkingTimeDAO.java
@@ -0,0 +1,34 @@
+package com.uade.psyline.infra.repository.mysql.dao;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "working_time")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class WorkingTimeDAO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private TherapistDAO therapist;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+}
