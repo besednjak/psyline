ed741c123cd6de1b395018786f363246e5ece8fb
besednjak <mario.besed@gmail.com>
2023-07-01 16:28:55 -0300

    Added patch and delete support for journal entries.

--- src/main/java/com/uade/psyline/application/usecase/PatientUsecase.java
+++ src/main/java/com/uade/psyline/application/usecase/PatientUsecase.java
@@ -1,5 +1,6 @@
 package com.uade.psyline.application.usecase;
 
+import com.uade.psyline.application.exception.JournalEntryNotFoundException;
 import com.uade.psyline.application.exception.PatientNotFoundException;
 import com.uade.psyline.application.service.PatientService;
 import com.uade.psyline.infra.repository.mysql.dao.JournalEntryDAO;
@@ -14,6 +15,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PatientUsecase implements PatientService {
@@ -71,11 +74,43 @@ public class PatientUsecase implements PatientService {
         return patientDTO;
     }
 
+    @Override
+    @Transactional
+    public PatientDTO updateJournalEntry(String userName, JournalEntryDTO updatedJournalEntryDTO) {
+        JournalEntryDAO existingJournalEntryDAO = this.findJournalEntryBy(updatedJournalEntryDTO.getId());
+        JournalEntryDAO updatedJournalEntryDAo = mapper.map(updatedJournalEntryDTO, JournalEntryDAO.class);
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        mapper.map(updatedJournalEntryDAo, existingJournalEntryDAO);
+        journalEntryRepository.save(existingJournalEntryDAO);
+
+        PatientDAO patientFoundDAO = this.findPatientByUserName(userName);
+        return mapper.map(patientFoundDAO, PatientDTO.class);
+    }
+
+    @Override
+    @Transactional
+    public PatientDTO deleteJournalEntry(String userName, Integer journalEntryId) {
+        JournalEntryDAO existingJournalEntryDAO = this.findJournalEntryBy(journalEntryId);
+        journalEntryRepository.delete(existingJournalEntryDAO);
+
+        PatientDAO patientFoundDAO = this.findPatientByUserName(userName);
+        return mapper.map(patientFoundDAO, PatientDTO.class);
+    }
+
     private PatientDAO findPatientByUserName(String userName) {
         PatientDAO patientFoundDAO = patientRepository.findPatientByUserName(userName);
         if(patientFoundDAO == null) {
             throw new PatientNotFoundException();
         }
         return patientFoundDAO;
+    }
+
+    private JournalEntryDAO findJournalEntryBy(Integer journalEntryId) {
+        Optional<JournalEntryDAO> journalEntryDAOFound = journalEntryRepository.findById(journalEntryId);
+        if(journalEntryDAOFound.isEmpty()) {
+            throw new JournalEntryNotFoundException();
+        }
+        return journalEntryDAOFound.get();
     }
 }
