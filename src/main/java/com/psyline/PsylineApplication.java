78c1ba47f21e03fe1465c852c4e98b12061b57cf
besednjak <besednjakmario@gmail.com>
2024-08-01 19:49:16 -0300

    init: initial scaffolding.

---
+++ src/main/java/com/psyline/PsylineApplication.java
@@ -0,0 +1,13 @@
+package com.psyline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsylineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsylineApplication.class, args);
+	}
+
+}
